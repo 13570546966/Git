e19a0a1c18a3f07da301e85b2d963e2b07cccadf
Leo <13570546966@163.com>
2022-08-23 06:40:23 +0800

    hot-fix01

--- src/n01/hello_world.java
+++ src/n01/hello_world.java
@@ -4,5 +4,6 @@ public class hello_world {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("next");
+        System.out.println("nextnext");
     }
 }
